2960f455fded1d73b4445f00e4eada2e10265767
EuforiaConR <euforiaconr@gmail.com>
2026-02-24 18:39:43 -0500

    tchulu behavior v2

--- BP/scripts/entities/eye_of_cthulhu.js
+++ BP/scripts/entities/eye_of_cthulhu.js
@@ -1,5 +1,6 @@
 import { world, system } from "@minecraft/server";
 import { Vec3 } from "../utils/vec3";
+import { Random } from "../utils/random";
 
 const ENTITY_TYPE_ID = "awaking_moonlord:eye_of_cthulhu";
 
@@ -10,19 +11,38 @@ world.afterEvents.dataDrivenEntityTrigger.subscribe((ev) => {
   console.warn(eventId);
 
   if (eventId === "awaking_moonlord:start_dash") {
-    const viewDir = entity.getViewDirection();
-    const normalizedViewDir = Vec3.normalize(viewDir);
-    const impulse = Vec3.scale(normalizedViewDir, 2); // Calculate impulse in the view direction
-    entity.applyImpulse(impulse); // Apply impulse in the view direction
+    let counter = 0;
+    const interval = system.runInterval(() => {
+      counter++
+      const viewDir = entity.getViewDirection();
+      const normalizedViewDir = Vec3.normalize(viewDir);
+
+      const impulse = Vec3.scale(normalizedViewDir, 1.2); // Calculate impulse in the opposite direction of the view
+
+      entity.clearVelocity();
+      entity.applyImpulse(impulse); // Apply an upward impulse to move away from the target
+      if (counter >= 10) {
+        system.clearRun(interval)
+      }
+    })
+
   } else if (eventId === "awaking_moonlord:start_move_away") {
-    const viewDir = entity.getViewDirection();
-    const normalizedViewDir = Vec3.normalize(viewDir);
+    // // Stop current movement
+    let counter = 0;
+    const randomInterval = Random.int(10, 20)
+    const interval = system.runInterval(() => {
+      counter++
+      const viewDir = entity.getViewDirection();
+      const normalizedViewDir = Vec3.normalize(viewDir);
 
-    const impulse = Vec3.scale(normalizedViewDir, -2.5); // Calculate impulse in the opposite direction of the view
+      const impulse = Vec3.scale(normalizedViewDir, -0.75); // Calculate impulse in the opposite direction of the view
 
-    //entity.clearVelocity(); // Stop current movement
-    //entity.applyImpulse({ x: 0, y: 1.5, z: 0 }); // Apply an upward impulse to move away from the target
-    entity.teleport(Vec3.add(entity.location, { x: 0, y: 15, z: 0 }))
+      entity.clearVelocity();
+      entity.applyImpulse({ x: impulse.x, y: 1.0, z: impulse.z }); // Apply an upward impulse to move away from the target
+      if (counter >= randomInterval) {
+        system.clearRun(interval)
+      }
+    })
   }
 });
 
